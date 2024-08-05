4f84f85b525e8eb655abe198302fe62d7d98eb84
Nicou11 <lyoungin1204@gmail.com>
2024-08-05 14:33:05 +0900

    add 9_12-2

--- src/extract/extract_9_12.py
+++ src/extract/extract_9_12.py
@@ -3,31 +3,31 @@ import pandas as pd
 import requests
 
 def get_key():
-    key=os.getenv("MOVIE_API_KEY")
+    key = os.getenv("MOVIE_API_KEY")
     return key
 
 def gen_url(load_dt='20160101'):
     base_url="http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
-    key=get_key()
-    url=f"{base_url}?key={key}&targetDt={load_dt}"
+    key = get_key()
+    url = f"{base_url}?key={key}&targetDt={load_dt}"
     return url
 
 def req(load_dt='20160101'):
-    url=gen_url(load_dt)
-    r=requests.get(url)
-    code=r.status_code
-    data=r.json()
+    url = gen_url(load_dt)
+    r = requests.get(url)
+    code = r.status_code
+    data = r.json()
     return code, data
 
 def req2list(load_dt='20160101'):
-    _, data=req(load_dt)
-    l=data['boxOfficeResult']['dailyBoxOfficeList']
-    df=pd.DataFrame(l)
+    _, data = req(load_dt)
+    l = data['boxOfficeResult']['dailyBoxOfficeList']
+    df = pd.DataFrame(l)
     return df
 
 def list2df(load_dt='20160101'):
-    l=req2list(load_dt)
-    df=pd.DataFrame(l)
+    l = req2list(load_dt)
+    df = pd.DataFrame(l)
     return df
 
 def save2df(load_dt='20160101'):
