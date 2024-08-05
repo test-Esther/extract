5427666e3af0de99a53fe2fa604dea8eb8f6cdde
mangG907 <mang.gee@gmail.com>
2024-08-05 17:09:27 +0900

    edit

--- src/extract/extract_5_8.py
+++ src/extract/extract_5_8.py
@@ -6,33 +6,36 @@ def get_key():
     key=os.getenv("MOVIE_API_KEY")
     return key
 
-def gen_url(load_dt='20160501'):
+def gen_url(load_dt='20160501', url_param={}):
     base_url="http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
     key=get_key()
     url=f"{base_url}?key={key}&targetDt={load_dt}"
+    for k, v in url_param.items():
+        url=url=f"&{k}={v}"
     return url
 
-def req(load_dt='20160501'):
-    url=gen_url(load_dt)
+def req(load_dt='20160501', url_param={}):
+    url=gen_url(load_dt, url_param)
     r=requests.get(url)
     code=r.status_code
     data=r.json()
     return code, data
 
-def req2list(load_dt):
-    _, data=req(load_dt)
+def req2list(load_dt, url_param={}):
+    _, data=req(load_dt, url_param)
     l=data['boxOfficeResult']['dailyBoxOfficeList']
     df=pd.DataFrame(l)
     return df
 
-def list2df(load_dt='20160501'):
-    l=req2list(load_dt)
+def list2df(load_dt='20160501', url_param={}):
+    l=req2list(load_dt, url_param)
     df=pd.DataFrame(l)
     return df
 
-def save2df(load_dt):
-    df=list2df(load_dt)
+def save2df(load_dt='20160501', url_param={}):
+    df=list2df(load_dt=load_dt, url_param=url_param)
     df['load_dt']=load_dt
+    df.to_parquet('~/tmp/team_parquet/', partition_cols=['load_dt'])
     return df
 
 def apply_type2df(load_dt, path="~/code/team_jkl/extract/extract_parquet"):
