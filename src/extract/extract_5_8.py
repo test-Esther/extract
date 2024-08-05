fe77df731a67dae584972fe801521dd349b536e5
mangG907 <mang.gee@gmail.com>
2024-08-05 17:32:48 +0900

    fix

--- src/extract/extract_5_8.py
+++ src/extract/extract_5_8.py
@@ -38,7 +38,7 @@ def save2df(load_dt='20160501', url_param={}):
     df.to_parquet('~/tmp/team_parquet/', partition_cols=['load_dt'])
     return df
 
-def apply_type2df(load_dt, path="~/code/team_jkl/extract/extract_parquet"):
+def apply_type2df(load_dt, path="~/tmp/team_parquet"):
     df=pd.read_parquet(f'{path}/load_dt={load_dt}')
     num_cols=['rnum', 'rank', 'rankInten', 'salesAmt', 'audiCnt', 'audiAcc', 'scrnCnt', 'showCnt', 'salesShare', 'salesInten', 'salesChange', 'audiInten', 'audiChange']
     df[num_cols]=df[num_cols].apply(pd.to_numeric)
