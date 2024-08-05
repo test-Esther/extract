134d7fd32020ef486ca85a5cd16d5adb64dffb37
Nicou11 <lyoungin1204@gmail.com>
2024-08-05 14:48:08 +0900

    add 9_12-4

--- src/extract/extract_9_12.py
+++ src/extract/extract_9_12.py
@@ -37,3 +37,13 @@ def save2df(load_dt='20160101'):
     df.to_parquet('~/tmp/team_parquet/', partition_cols=['load_dt'])
     return df
 
+def apply_type2df(load_dt="20160101", path="~/tmp/team_parquet"):
+    df = pd.read_parquet(f'{path}/load_dt={load_dt}')
+    num_cols = ['rnum', 'rank', 'rankInten', 'salesAmt', 'audiCnt', 'audiAcc', 'scrnCnt', 'showCnt', 'salesShare', 'salesInten', 'salesChange', 'audiInten', 'audiChange']
+
+    #for c in num_cols:
+        #df[c] = pd.to_numeric(df[c])
+
+    df[num_cols] = df[num_cols].apply(pd.to_numeric)
+
+    return df
